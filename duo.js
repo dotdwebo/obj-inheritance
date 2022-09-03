da160bd8334af13c267b59eb7164d0f27a8c9d93
Dwebo <100784480+dotdwebo@users.noreply.github.com>
2022-09-04 03:53:33 +0700

    Add files via upload

---
+++ duo.js
@@ -0,0 +1,10 @@
+function req(item){
+    console.log(`I am gonna request something! It is ${item}`);
+    
+}
+
+function res(item){
+    console.log(`I am gonna respond to something! It is ${item}`);
+}
+
+export {req, res};
---
+++ reqres.js
@@ -0,0 +1,4 @@
+import {res, req} from './duo.js';
+
+req('money');
+res('your request');
