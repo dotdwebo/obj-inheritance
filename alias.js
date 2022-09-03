de90d716591548fb1681d10e1f687a2f439edb6d
Dwebo <100784480+dotdwebo@users.noreply.github.com>
2022-09-04 04:12:51 +0700

    Add files via upload

---
+++ alias.js
@@ -0,0 +1,6 @@
+import {lipsum, consec} from './exportas.js';
+
+
+lipsum('Thomas Shelby');
+consec('Salvador Dali');
+
---
+++ exportas.js
@@ -0,0 +1,9 @@
+function loremipsumdolorsitamet(name){
+    console.log(`My name is ${name} and Lorem Ipsum Dolor Sit Amet is my mantra!`)
+}
+
+function consecteturadipiscingelite(name){
+    console.log(`My name is ${name} and Consectetur Adipiscing Elite is my mantra!`);
+}
+
+export {loremipsumdolorsitamet as lipsum, consecteturadipiscingelite as consec}; //using alias
